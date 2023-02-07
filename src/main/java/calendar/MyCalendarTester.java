8ea43ed44ff98a52f188953f3795fddc3b797bf5
Eometheous <jonathan.sthomas@icloud.com>
2023-02-06 17:51:42 -0800

    Used LocalTime.parse and LocalDate.parse for getting dates and times from events.txt

--- src/main/java/calendar/MyCalendarTester.java
+++ src/main/java/calendar/MyCalendarTester.java
@@ -171,57 +171,25 @@ public class MyCalendarTester {
 
                 information = information.toUpperCase();
 
+                DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("M/d/yy");
+                DateTimeFormatter hourMinute = DateTimeFormatter.ofPattern("H:m");
+
                 String[] splitInfo = information.split(" ");
                 if (splitInfo[0].contains("S") || splitInfo[0].contains("M") || splitInfo[0].contains("T") || splitInfo[0].contains("W")
                         || splitInfo[0].contains("R") || splitInfo[0].contains("F") || splitInfo[0].contains("A") ) {
-                    String[] splitStartTime = splitInfo[1].split(":");
-                    String[] splitEndTime = splitInfo[2].split(":");
 
-                    int hour = Integer.parseInt(splitStartTime[0]);
-                    int minute = Integer.parseInt(splitStartTime[1]);
-                    LocalTime startTime = LocalTime.of(hour, minute);
+                    LocalTime startTime = LocalTime.parse(splitInfo[1],hourMinute);
+                    LocalTime endTime = LocalTime.parse(splitInfo[2], hourMinute);
 
-                    hour = Integer.parseInt(splitEndTime[0]);
-                    minute = Integer.parseInt(splitEndTime[1]);
-                    LocalTime endTime = LocalTime.of(hour, minute);
-
-                    String[] splitStartDate = splitInfo[3].split("/");
-                    int startMonth, startDay, startYear, endMonth, endDay, endYear;
-                    startMonth = Integer.parseInt(splitStartDate[0]);
-                    startDay = Integer.parseInt(splitStartDate[1]);
-                    startYear = Integer.parseInt(splitStartDate[2]);
-                    startYear += 2000;
-
-                    String[] splitEndDate = splitInfo[4].split("/");
-                    endMonth = Integer.parseInt(splitEndDate[0]);
-                    endDay = Integer.parseInt(splitEndDate[1]);
-                    endYear = Integer.parseInt(splitEndDate[2]);
-                    endYear += 2000;
-
-                    LocalDate startDate = LocalDate.of(startYear, startMonth, startDay);
-                    LocalDate endDate = LocalDate.of(endYear, endMonth, endDay);
+                    LocalDate startDate = LocalDate.parse(splitInfo[3], monthDayYear);
+                    LocalDate endDate = LocalDate.parse(splitInfo[4], monthDayYear);
 
                     myCalendar.addRecurringEvent(name, splitInfo[0], startDate, endDate, startTime, endTime);
                 }
                 else {
-                    LocalDate date;
-                    int hour, minute, year, month, day;
-                    String[] splitDate = splitInfo[0].split("/");
-                    year = Integer.parseInt(splitDate[2]);
-                    year += 2000;
-                    month = Integer.parseInt(splitDate[0]);
-                    day = Integer.parseInt((splitDate[1]));
-                    date = LocalDate.of(year, month, day);
-
-                    String[] splitStartTime = splitInfo[1].split(":");
-                    hour = Integer.parseInt(splitStartTime[0]);
-                    minute = Integer.parseInt(splitStartTime[1]);
-                    LocalTime startTime = LocalTime.of(hour, minute);
-
-                    String[] splitEndTime = splitInfo[2].split(":");
-                    hour = Integer.parseInt(splitEndTime[0]);
-                    minute = Integer.parseInt(splitEndTime[1]);
-                    LocalTime endTime = LocalTime.of(hour, minute);
+                    LocalDate date = LocalDate.parse(splitInfo[0], monthDayYear);
+                    LocalTime startTime = LocalTime.parse(splitInfo[1], hourMinute);
+                    LocalTime endTime = LocalTime.parse(splitInfo[2], hourMinute);
 
                     myCalendar.addEvent(name, date, startTime, endTime);
                 }
