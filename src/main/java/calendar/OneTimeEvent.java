7e577af26cf57997e0125cddf84c789947dfe35d
Eometheous <jonathan@sthomasphoto.com>
2023-02-08 16:54:53 -0800

    now implements Comparable

--- src/main/java/calendar/OneTimeEvent.java
+++ src/main/java/calendar/OneTimeEvent.java
@@ -4,20 +4,24 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class OneTimeEvent extends Event {
+public class OneTimeEvent extends Event implements Comparable<OneTimeEvent>{
     private final LocalDate date;
     public OneTimeEvent(String n, LocalDate d, LocalTime sT, LocalTime eT) {
         super(n, sT, eT);
         date = d;
     }
-
-    public OneTimeEvent(String n, LocalDate d, TimeInterval timeInterval) {
-        super(n, timeInterval);
-        date = d;
-    }
     public LocalDate getDate() {return date;}
+
+    @Override
+    public int compareTo(OneTimeEvent o) {
+        if (date.isBefore(o.getDate()) && timeInterval.getStart().isBefore(o.getTimeInterval().startTime)) return -1;
+        else if (date.isAfter(o.getDate()) && timeInterval.getStart().isBefore(o.getTimeInterval().startTime)) return 1;
+        return 0;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s %s", timeInterval, name);
+        DateTimeFormatter dayMonthDay = DateTimeFormatter.ofPattern("EEEE, MMMM d");
+        return String.format("%s\n\t%s on %s", name, timeInterval, dayMonthDay.format(date));
     }
 }
