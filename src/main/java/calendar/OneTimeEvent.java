65656c6bf167d9a8b4b33431d2e0630110ab0fd5
Eometheous <jonathan@sthomasphoto.com>
2023-02-08 20:07:07 -0800

    Added save() which is used when saving to output.txt

--- src/main/java/calendar/OneTimeEvent.java
+++ src/main/java/calendar/OneTimeEvent.java
@@ -12,6 +12,11 @@ public class OneTimeEvent extends Event implements Comparable<OneTimeEvent>{
     }
     public LocalDate getDate() {return date;}
 
+    public String save() {
+        DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("M/d/yy");
+        return String.format("%s\n%s %s\n",name, monthDayYear.format(date), timeInterval);
+    }
+
     @Override
     public int compareTo(OneTimeEvent o) {
         if (date.isBefore(o.getDate()) && timeInterval.getStart().isBefore(o.getTimeInterval().startTime)) return -1;
@@ -22,6 +27,6 @@ public class OneTimeEvent extends Event implements Comparable<OneTimeEvent>{
     @Override
     public String toString() {
         DateTimeFormatter dayMonthDay = DateTimeFormatter.ofPattern("EEEE, MMMM d");
-        return String.format("%s\n\t%s on %s", name, timeInterval, dayMonthDay.format(date));
+        return String.format("%s\n\t%s on %s\n", name, timeInterval, dayMonthDay.format(date));
     }
 }
