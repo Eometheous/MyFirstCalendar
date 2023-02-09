9800144a01cfeca81aba58d91b01adfadbef956c
Eometheous <jonathan@sthomasphoto.com>
2023-02-08 16:53:15 -0800

    removed Comparable implementation

--- src/main/java/calendar/Event.java
+++ src/main/java/calendar/Event.java
@@ -2,7 +2,7 @@ package calendar;
 
 import java.time.LocalTime;
 
-abstract class Event implements Comparable<Event> {
+abstract class Event {
     protected String name;
     protected TimeInterval timeInterval;
 
@@ -14,17 +14,6 @@ abstract class Event implements Comparable<Event> {
     public Event(String name, TimeInterval timeInterval) {
         this.name = name;
         this.timeInterval = timeInterval;
-    }
-
-    /**
-     * Compares this event with event e
-     * @param e the object to be compared.
-     * @return  -1 if it is before this event, 1 if it is after this event, and 0 if it is the same day as this event
-     */
-    public int compareTo(Event e) {
-        if (timeInterval.getStart().isBefore(e.getTimeInterval().getStart())) return -1;
-        else if (timeInterval.getStart().isAfter(e.getTimeInterval().getStart())) return 1;
-        return 0;
     }
 
     public TimeInterval getTimeInterval() {return timeInterval;}
