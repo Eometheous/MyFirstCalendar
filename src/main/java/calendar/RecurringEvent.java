55ea03b4588dd5b6b6590e71fbff2ae7b920a482
Eometheous <jonathan@sthomasphoto.com>
2023-02-09 19:01:41 -0800

    Added JavaDoc Changed argument names in RecurringEvent constructor Changed name of save to inFormatMonthDayYear

--- src/main/java/calendar/RecurringEvent.java
+++ src/main/java/calendar/RecurringEvent.java
@@ -1,16 +1,39 @@
 package calendar;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+/**
+ * An {@code Event} that is recurring.
+ * A {@code RecurringEvent} takes place on multiple days of the week and repeats weekly
+ * @see calendar.Event
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230209
+ */
 public class RecurringEvent extends Event implements Comparable<RecurringEvent>{
     private final ArrayList<LocalDate> dates;
     private final String days;
 
-    public RecurringEvent(String n, String days, LocalDate startDate, LocalDate endDate, LocalTime sT, LocalTime eT) {
-        super(n, sT, eT);
+    /**
+     * Creates a {@code RecurringEvent}.
+     * A recurring event has a name, days of the week it recurs on,
+     * a start date, end date, and start  time, and end time.
+     * @param name                  name of the event
+     * @param days                  days of the week the event takes place on
+     * @param startDate             date the event starts recurring
+     * @param endDate               date the event stops recurring
+     * @param startTime             time the event starts
+     * @param endTime               time the event ends
+     * @throws DateTimeException    if the start date is after the end date
+     */
+    public RecurringEvent(String name, String days, LocalDate startDate,
+                          LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+        super(name, startTime, endTime);
+        if (startDate.isAfter(endDate)) throw new DateTimeException("Start date must be before end date");
+
         dates = new ArrayList<>();
         this.days = days;
         int[] recurringDays = convertDaysToDayOfWeekValue(days);
@@ -36,6 +59,12 @@ public class RecurringEvent extends Event implements Comparable<RecurringEvent>{
             }
         }
     }
+
+    /**
+     * Converts a String of day abbreviations into integers referring to the day of the week.
+     * @param days  String of day abbreviations
+     * @return  integer array of days of the week values
+     */
     private int[] convertDaysToDayOfWeekValue(String days) {
         // Converting the String days into integers for the day of the week
         char[] daysToCharArr = days.toCharArray();
@@ -52,27 +81,80 @@ public class RecurringEvent extends Event implements Comparable<RecurringEvent>{
         return recurringDays;
     }
 
+    /**
+     * Gets the dates the {@code RecurringEvent} takes place on.
+     * @return  the dates.
+     */
     public ArrayList<LocalDate> getDates() {return dates;}
+
+    /**
+     * Gets the date the {@code RecurringEvent} starts recurring
+     * @return  the starting date
+     */
     public LocalDate getStartDate() {return dates.get(0);}
+
+    /**
+     * Gets the date the {@code RecurringEvent} stops recurring
+     * @return  the ending date
+     */
     public LocalDate getEndDate() {return dates.get(dates.size() - 1);}
 
-    public String save() {
+    /**
+     * Outputs {@code RecurringEvent} as a String using the {@code M/d/yy} pattern for the
+     * start and end dates.
+     * @return  String of {@code RecurringEvent}
+     */
+    public String inFormatMonthDayYear() {
         DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("M/d/yy");
         return String.format("%s\n%s %s %s %s\n", name,
                 days, monthDayYear.format(getStartDate()), monthDayYear.format(getEndDate()), timeInterval);
     }
 
+    /**
+     * From implementation of {@code Comparable}.
+     * Compares this {@code RecurringEvent} with another {@code RecurringEvent}.
+     * If this {@code RecurringEvent} is before the other {@code RecurringEvent},
+     * it takes place before the other one. If {@code RecurringEvent} is after the
+     * other {@code RecurringEvent}, it takes place after the other one.
+     * @see Comparable
+     * @param otherEvent the other {@code RecurringEvent} being compared
+     * @return {@code -1} if before and {@code 1} if after
+     */
     @Override
-    public int compareTo(RecurringEvent o) {
-        if (getStartDate().isBefore(o.getStartDate()) && timeInterval.getStart().isBefore(o.getTimeInterval().getStart())) return -1;
-        else if (getStartDate().isAfter(o.getStartDate()) && timeInterval.getStart().isAfter(o.getTimeInterval().getStart())) return 1;
-        return 0;
+    public int compareTo(RecurringEvent otherEvent) {
+        if (getStartDate().isBefore(otherEvent.getStartDate()) || startTimeIsBefore(otherEvent)) return -1;
+        else if (getStartDate().isAfter(otherEvent.getStartDate()) || startTimeIsAfter(otherEvent)) return 1;
+        return 0;    // can't really happen, but is needed anyway
     }
 
+    /**
+     * A helper function used in {@code compareTo} to check if the start time of this
+     * is after {@code otherEvent}.
+     * @param otherEvent    the event being compared to.
+     * @return              true if this {@code RecurringEvent} starts after the other event.
+     */
+    private boolean startTimeIsAfter(RecurringEvent otherEvent) {
+        return timeInterval.getStart().isAfter(otherEvent.timeInterval.getStart());
+    }
+
+    /**
+     * A helper function used in {@code compareTo} to check if the start time of this
+     * is before {@code otherEvent}.
+     * @param otherEvent    the event being compared to.
+     * @return              true if this {@code RecurringEvent} starts before the other event.
+     */
+    private boolean startTimeIsBefore(RecurringEvent otherEvent) {
+        return timeInterval.getStart().isBefore(otherEvent.timeInterval.getStart());
+    }
+
+    /**
+     * Outputs {@code RecurringEvent} as a String using the date format {@code M/d/yy}.
+     * @return  String of {@code RecurringEvent}.
+     */
     @Override
     public String toString() {
         DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("M/d/yy");
-        return String.format("%s\n\tEvery %s from %s from %s to %s\n", name, days, timeInterval,
+        return String.format("%s: %s every %s from %s to %s\n", name, timeInterval, days,
                 monthDayYear.format(getStartDate()), monthDayYear.format(getEndDate()));
     }
 }
