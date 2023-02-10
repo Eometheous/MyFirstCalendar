dde4bb9f7f07f8bc4c459424aacf548bcc36c66d
Eometheous <jonathan@sthomasphoto.com>
2023-02-09 17:40:10 -0800

    Added JavaDoc Changed argument names in Event constructor

--- src/main/java/calendar/Event.java
+++ src/main/java/calendar/Event.java
@@ -2,14 +2,24 @@ package calendar;
 
 import java.time.LocalTime;
 
+/**
+ * An event without a specific date.
+ * {@code Event} holds a name and a {@code TimeInterval}
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230209
+ */
 abstract class Event {
     protected String name;
     protected TimeInterval timeInterval;
 
-    public Event(String name, LocalTime sT, LocalTime eT) {
+    /**
+     * Creates an {@code Event} with a name, start time and end time
+     * @param name      the name of the event
+     * @param startTime start time of event
+     * @param endTime   end time of event
+     */
+    public Event(String name, LocalTime startTime, LocalTime endTime) {
         this.name = name;
-        this.timeInterval = new TimeInterval(sT, eT);
+        this.timeInterval = new TimeInterval(startTime, endTime);
     }
-
-    public TimeInterval getTimeInterval() {return timeInterval;}
 }
